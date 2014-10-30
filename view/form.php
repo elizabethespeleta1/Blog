8f12136d7f3d240d58a37e02935c246d4dff8929
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2014-10-30 11:27:15 -0700

    commented on code for video 19

--- view/form.php
+++ view/form.php
@@ -1,13 +1,20 @@
+<!-- indicates what your about to see -->
 <h1> Create Blog Post </h1>
 
 <form>
+<!--form it'll take info and send it to the table -->
+	<!-- first div is for title -->
 	<div>
+		<!--label indicates what it is -->
 		<label for="title">Title: </label>
+		<!--input is for text usually for one line -->
 		<input type="text" name="title"/>
 	</div>
 
+	<!--second div if for post -->
 	<div>
 		<label for="post">Post: </label>
+		<!--text area is for allows us to type a lot of text -->
 		<textarea name="post"></textarea>
 	</div>
 </form>
