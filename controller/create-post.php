fb9966a051495e42fdfe34f17218ae1b420beb27
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2014-10-30 17:48:19 -0700

    commented on code for video 21

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,4 +1,6 @@
 <?php
+	//filer_input it validates and sanitizes the inputs
+	//post in the () means is info posting somewhere
 	$title = filer_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filer_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
