a4357e898686f606252aefcb249a8787f28cbd8c
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2014-10-30 18:18:04 -0700

    finished video 23

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,4 +1,9 @@
 <?php
+
+	require_once(__DIR__ . "/../model/database.php");
+	$connection = new mysqli($host,$username,$password,$database);
+
+
 	//filer_input it validates and sanitizes the inputs
 	//post in the () means is info posting somewhere
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
@@ -6,3 +11,5 @@
 
 	echo "<p>Title: $title</p>";
 	echo "<p>Post: $post</p>";
+
+	$connection->close();
