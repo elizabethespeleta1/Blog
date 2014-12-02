9135ccc4be1d62d6f7a30812563ad40cd812cce2
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2014-12-02 10:57:56 -0800

    finished and commented on code for video 37

---
+++ view/register-form.php
@@ -0,0 +1,26 @@
+<form>
+	<!-- each div is a section-->
+	<div>
+		<!-- the label is for the email -->
+		<label for="email"> Email:</label>
+		<input type="text" name="email" />
+	</div>
+
+	<div>
+		<!-- the label is for the username-->
+		<label for="username"> Username:</label>
+		<input type="text" name="username" />
+	</div>
+
+	<div>
+		<!-- the label is for the password-->
+		<label for="password"> Password:</label>
+		<!-- this unput is different so the password doesn't show text-->
+		<input type="password" name="password" />
+	</div>
+
+	<div>
+		<button type="submit">Submit</button>
+	</div>
+
+</form>
