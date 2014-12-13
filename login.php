4f7c263bbc4a36e579b1900a87db448cbe93e7ab
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2014-12-13 15:18:24 -0800

    finished up to video 46 and commented on code

---
+++ login.php
@@ -0,0 +1,7 @@
+<?php 
+	//to access header from the view
+	require_once(__DIR__ . "/view/header.php");
+	//links to the login form
+	require_once(__DIR__ . "/view/login-form.php");
+	//closing the page with the footer
+	require_once(__DIR__ . "/view/footer.php");
---
+++ view/login-form.php
@@ -0,0 +1,29 @@
+<?php
+  require_once(__DIR__ . "/../model/config.php")
+?>
+
+<!-- this is just to label our page-->
+<h1>Login</h1>
+
+<!-- method is post because this is how we send the info-->
+<!-- -->
+<form method="post" action="<?php echo $path . "controller/login-user.php" ?>">
+
+<div>
+		<!-- the label is for the username-->
+		<label for="username"> Username:</label>
+		<input type="text" name="username" />
+	</div>
+
+	<div>
+		<!-- the label is for the password-->
+		<label for="password"> Password:</label>
+		<!-- this unput is different so the password doesn't show text-->
+		<input type="password" name="password" />
+	</div>
+
+	<div>
+		<button type="submit">Submit</button>
+	</div>
+
+ </form>
