0ea9b35f8fca8f9155d7894d4539deb73da61dbd
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2014-12-15 19:46:12 -0800

    finished video 50 and commented on code

--- index.php
+++ index.php
@@ -10,15 +10,26 @@
 //view is probably where you'll get info from the model or have html code
 //* note __DIR__ concatinates
 
-//this allows you to display posts from the database
-	require_once(__DIR__ . "/controller/read-posts.php");
-// it has inserted code from header and putting it in index php
+	//gives access to the function in login-verify
+	require_once(__DIR__ . "/controller/login-verify.php");
+
+	// it has inserted code from header and putting it in index php
 	require_once(__DIR__ . "/view/header.php");
-//it has inserted code from footer and putting it in index php
-	require_once(__DIR__ . "/view/footer.php");
-//this allows the link to show up
+
+	//if statement so navigation only displays when user is logged in
+	if(authenticateUser()){
+
+	//this allows the link to show up
 	require_once(__DIR__ . "/view/navigation.php");
+	}
+
 	require_once(__DIR__ . "/controller/create-db.php");
+
+	//it has inserted code from footer and putting it in index php
+	require_once(__DIR__ . "/view/footer.php");
+
+	//this allows you to display posts from the database
+	require_once(__DIR__ . "/controller/read-posts.php");
 ?>
 
 <!-- separating everything helps make it easier to maintain our code-->
